a52fde723a5e9b37d62452c2ebc415a8516a227f
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-05-02 15:43:42 +0900

    Create enter-eks.sh

---
+++ enter-eks.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+KEY=$1
+
+GROUP_ID=$(aws ec2 describe-security-groups | jq -r '.SecurityGroups[] | select(.GroupName | startswith("myeks")).GroupId')
+
+OLD_CIDR=$(aws ec2 describe-security-groups --group-id $GROUP_ID | jq -r '.SecurityGroups[].IpPermissions[].IpRanges[].CidrIp')
+
+NEW_CIDR=$(curl https://ipinfo.io/ip)/32
+
+RULE_ID=$(aws ec2 describe-security-group-rules --filters "Name=group-id,Values=$GROUP_ID" | jq -r '.SecurityGroupRules[] | select(.CidrIpv4 | startswith("'$OLD_CIDR'")).SecurityGroupRuleId')
+
+aws ec2 modify-security-group-rules --group-id $GROUP_ID --security-group-rules '[{"SecurityGroupRuleId":"'$RULE_ID'","SecurityGroupRule":{"IpProtocol":"-1","CidrIpv4":"'$NEW_CIDR'"}}]' > /dev/null
+
+ssh -i $KEY ec2-user@$(aws ec2 describe-instances --filters "Name=instance.group-name,Values=myeks*" --query "Reservations[].Instances[].PublicIpAddress" --output text)
